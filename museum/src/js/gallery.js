00f6d06e205cd5705fe575c14a3f9e6948f388f0
Mark Pokrovsky <solexofficial@yandex.ru>
2021-09-27 23:27:37 +0300

    feat: random images gallery section

--- museum/src/js/gallery.js
+++ museum/src/js/gallery.js
@@ -1,1 +1,12 @@
-console.log('gallery');
+// utils
+function shuffle(array) {
+  return array.sort(() => Math.random() - 0.5);
+}
+
+const $galleryContainer = document.querySelector('#gallery-container');
+const $galleryImages = document.querySelectorAll('.gallery-img');
+const shuffleImages = shuffle([...$galleryImages]);
+
+for (let i = 0; i < shuffleImages.length; i++) {
+  $galleryContainer.appendChild(shuffleImages[i]);
+}
