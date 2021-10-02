bc1c686bc45ad88ca6dd3f2a2f13cc2661821600
Mark Pokrovsky <solexofficial@yandex.ru>
2021-10-02 15:06:16 +0300

    feat: add pixel perfect welcome section

--- museum/src/js/burger-menu.js
+++ museum/src/js/burger-menu.js
@@ -2,8 +2,18 @@ const $menuBtn = document.querySelector('#burger-menu');
 const $menu = document.querySelector('#nav-menu');
 const $welcomeContent = document.querySelector('#welcome-content');
 
-$menuBtn.addEventListener('click', () => {
+$menuBtn.addEventListener('click', () => toggleMenu());
+
+document.querySelectorAll('.navigation-nav__link').forEach(el => el.addEventListener('click', () => toggleMenu()));
+
+document.body.addEventListener('click', e => {
+  if (e.target !== $menu) {
+    console.log('close');
+  }
+});
+
+const toggleMenu = () => {
   $menuBtn.classList.toggle('active');
   $menu.classList.toggle('active');
   $welcomeContent.classList.toggle('hide');
-});
+};
