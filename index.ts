5e88151c4f0aa656328bf08ebe684ca8329846ea
Mark Pokrovsky <solexofficial@yandex.ru>
2022-04-01 22:08:19 +0300

    добавляет пример использования enum

--- index.ts
+++ index.ts
@@ -1,20 +1,12 @@
-function sum(a: number, b: number): number {
+enum StatusCodes {
+  NotFound = 404,
+  ServerError = 500,
+}
+
+function sum(a: StatusCodes, b: StatusCodes): StatusCodes {
   return a + b;
 }
 
-sum(1, 3);
+sum(StatusCodes.ServerError, StatusCodes.NotFound);
 
-enum Foo {
-  A,
-  B,
-  C,
-}
-
-const Foo2 = {
-  A: 0,
-  B: 1,
-  C: 2,
-  0: 'A',
-  1: 'B',
-  2: 'C',
-};
+StatusCodes.ServerError[500] = 'ServerError';
