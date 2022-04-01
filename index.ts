b9b5a31e2262dd3194c27a0ae6f8fd55214847c7
Mark Pokrovsky <solexofficial@yandex.ru>
2022-04-01 21:11:26 +0300

    добавляет вариант комментария js doc

--- index.ts
+++ index.ts
@@ -1,29 +1,14 @@
-const hello = 'world';
-console.log(hello);
+//typeof, instanceof - интроспекция
 
-function foo(a: string, b: number): number {
-  return Number(a) + b;
+/**
+ * @param {number} a
+ * @param {number} b
+ * @return {number}
+ *
+ */
+
+function sum(a, b) {
+  return a + b;
 }
 
-foo('2', 5).toFixed();
-typeof NaN;
-
-// number // double IEE754 -> memory 64bit
-// bigInt
-
-// string UTF-16 (each symbol 16bit), emoji - суррогатная пара, 2 символа.
-function tag() {
-  console.log(arguments);
-}
-
-// tag(`34334 ${1 + 2}`);
-
-// boolean
-// true 0000 0001
-// false 0000 0000
-
-//symbol
-// null
-// undefined
-
-//object
+sum(1, '34');
