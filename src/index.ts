5eef4a1694c8580fcce41ed2053e088a5cbfc6b7
Mark Pokrovsky <solexofficial@yandex.ru>
2022-04-02 14:59:58 +0300

    добавляет базовые типы, подтипы объектов, свои типы с помощью классов

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,28 @@
+let a: number = 1;
+let b: bigint = 1n;
+let c: string = 'asgasg';
+let d: symbol = Symbol();
+let e: boolean = true;
+let f: null = null;
+let g: undefined = undefined;
+let o: object = [];
 
+class MyArray extends Array {}
 
+function foo(arr: string[]) {}
+
+foo(new MyArray());
+
+class Bla {
+  bar: string = '123';
+
+  foo(): number {
+    return 1;
+  }
+}
+
+// function getBla(a: typeof Bla) {}
+function getBla(a: Bla) {}
+
+// getBla(Bla);
+getBla(new Bla());
